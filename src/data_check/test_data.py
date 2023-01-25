9acda8c5c11dd99eb6bd51ac0b37a307cbd66fd9
Paulo Souza <paulo.souza@experian.com>
2023-01-25 17:12:11 -0300

    Finished data_check step.

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
-
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test if the size of the dataset is big enough to ensure a proper training population,
+    but also not too large to incur in OOM errors
+    """
+    assert 15000 < data.shape[0] < 1000000
+    
+def test_price_range(data, min_price, max_price):
+    """
+    Ensure that all prices being fed into the model are within the specified range
+    """
+    assert all(data['price'].between(min_price, max_price))
