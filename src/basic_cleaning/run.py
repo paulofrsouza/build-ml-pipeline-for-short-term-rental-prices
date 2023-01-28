11024b9c8833361898be363b16d43aa6a37b37c1
Paulo Souza <paulo.souza@experian.com>
2023-01-28 13:00:38 -0300

    Adding geolocational data cleaning.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,8 @@ def go(args):
     idx = df['price'].between(float(args.min_price), float(args.max_price))
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info('Saving and exporting cleaned data.')
     df.to_csv('clean_sample.csv', index=False)
